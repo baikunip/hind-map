a46bb5ede528f508bb1f35c9fb2ed491c9b6bdca
kuni <ahmad.baikuni17@pwk.undip.ac.id>
2024-11-09 23:34:26 +0700

    add map control

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,22 @@ const map = new mapboxgl.Map({
     minZoom:2,
     maxZoom:2
 });
+// Add zoom and rotation controls to the map.
+class HelloWorldControl {
+    onAdd(map) {
+        this._map = map;
+        this._container = document.createElement('div');
+        this._container.className = 'mapboxgl-ctrl';
+        this._container.textContent = 'Hello, world';
+        return this._container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+}
+map.addControl(new mapboxgl.NavigationControl());
 let hoveredPolygonId = null,hideFilter=true
 map.on('load',()=>{
     map.addSource('continents',{
