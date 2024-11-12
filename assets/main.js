81d3c5e32bed6e6f50f79a65835fb59b481d1597
kuni <56590656+baikunip@users.noreply.github.com>
2024-11-12 15:28:23 +0700

    add popup window

--- assets/main.js
+++ assets/main.js
@@ -34,11 +34,13 @@ let hoveredPolygonId = null,hideFilter=true
 function loadLayers(){
     map.addSource('continents',{
         'type': 'geojson',
-        'data':'https://r2.datahub.io/clvyjaryy0000la0cxieg4o8o/main/raw/data/countries.geojson'
+        'data':'https://r2.datahub.io/clvyjaryy0000la0cxieg4o8o/main/raw/data/countries.geojson',
+        'generateId': true //This ensures that all features have unique IDs 
     })
     map.addSource('datapoints',{
         'type':'geojson',
-        'data':datapoints
+        'data':datapoints,
+        'generateId': true //This ensures that all features have unique IDs 
     })
     map.addLayer({
         'id': 'continents-layer',
@@ -56,120 +58,158 @@ function loadLayers(){
             }
         })
     map.addLayer({
-        'id': 'point-layer',
+            'id': 'point-layer',
             'type': 'circle',
             'source': 'datapoints',
             'paint': {
                 'circle-radius': 10,
-                'circle-color': '#F84C4C' // red color
+                'circle-color': 'rgb(88, 182, 150)' // red color
             }
     })
 }
 map.on('load',()=>{
     loadLayers()
-    map.on('click', 'continents-layer', (e) => {
-        map.setProjection('mercator')
-        map.setMaxZoom(18)
-        map.zoomTo(5)
-        map.setMinZoom(2)
-    })
-    // the mouse is over the states layer.
-    map.on('mouseenter', 'continents-layer', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-      // feature state for the feature under the mouse.
-      map.on('mousemove', 'continents-layer', (e) => {
-        if (e.features.length > 0) {
+    map.on('idle',()=>{
+        map.once('click', 'continents-layer', () => {
+            if(map.getZoom()<3){ 
+                map.setProjection('mercator')
+                map.setMaxZoom(18)
+                map.zoomTo(5)
+                map.setMinZoom(2)
+            }           
+            map.on('click',(e) => {
+                let features = map.queryRenderedFeatures(e.point, { layers: ['continents-layer']});
+                if (!features.length) {
+                    $('#popup-window-buttons').hide()
+                    $('.window-caption').attr('display','none')
+                    return;
+                }
+                $('#popup-window-buttons').attr('data-title',features[0].properties.ADMIN+' Popup')
+                $('.window-caption').attr('display','flex')
+                $('#popup-window-buttons').show()
+                map.panTo(e.lngLat)
+                new mapboxgl.Popup({ closeOnClick: false })
+                        .setLngLat(e.lngLat)
+                        .setHTML(`
+                                <div class="social-box"style="padding:10px;">
+                                    <div class="header bg-cyan fg-white">
+                                        <div class="title">`+features[0].properties.ADMIN+`</div>
+                                        <div class="subtitle">`+features[0].properties.ISO_A3+`</div>
+                                    </div>
+                                    <ul class="skills">
+                                        <li>
+                                            <div class="text-bold">6</div>
+                                            <div>VALUE</div>
+                                        </li>
+                                        <li>
+                                            <div class="text-bold">4</div>
+                                            <div>VALUE</div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            `)
+                        .addTo(map);
+            })
+        })
+        // the mouse is over the states layer.
+        map.on('mouseenter', 'continents-layer', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+          // feature state for the feature under the mouse.
+        map.on('mousemove', 'continents-layer', (e) => {
+            if (e.features.length > 0) {
+                if (hoveredPolygonId !== null) {
+                    map.setFeatureState(
+                        { source: 'continents', id: hoveredPolygonId },
+                        { hover: false }
+                    );
+                }
+                hoveredPolygonId = e.features[0].properties["ADMIN"];
+                map.setFeatureState(
+                    { source: 'continents', id: hoveredPolygonId },
+                    { hover: true }
+                );
+            }
+        });
+        // Change the cursor back to a pointer
+        // when it leaves the states layer.
+        map.on('mouseleave', 'continents-layer', () => {
+            map.getCanvas().style.cursor = '';
             if (hoveredPolygonId !== null) {
                 map.setFeatureState(
                     { source: 'continents', id: hoveredPolygonId },
                     { hover: false }
                 );
             }
-            hoveredPolygonId = e.features[0].properties["CONTINENT"];
-            map.setFeatureState(
-                { source: 'continents', id: hoveredPolygonId },
-                { hover: true }
-            );
-        }
-    });
-    // Change the cursor back to a pointer
-    // when it leaves the states layer.
-    map.on('mouseleave', 'continents-layer', () => {
-        map.getCanvas().style.cursor = '';
-        if (hoveredPolygonId !== null) {
-            map.setFeatureState(
-                { source: 'continents', id: hoveredPolygonId },
-                { hover: false }
-            );
-        }
-        hoveredPolygonId = null;
-    });
-    $('input[type=radio][name=basemap]').change(function() {
-        map.setStyle('mapbox://styles/mapbox/' +this.value)
-    })
-    // filters
-    $('#strategy-checkboxes').on('change',(val)=>{
-        if(val.isTrusted){
-            switch (val.target.dataset.caption) {
-                case "Adapt":
-                    strategyAdaptation=="Adapt"?strategyAdaptation=null:strategyAdaptation="Adapt"
-                    break;
-                case "Prepare":
-                    strategyPreparation=="Prepare"?strategyPreparation=null:strategyPreparation="Prepare"
-                    break;
-                default:
-                    strategyAnticipation=="Anticipate"?strategyAnticipation=null:strategyAnticipation="Anticipate"
-                    break;
+            hoveredPolygonId = null;
+        });
+        $('input[type=radio][name=basemap]').change(function() {
+            map.setStyle('mapbox://styles/mapbox/' +this.value)
+        })
+        // filters
+        $('#strategy-checkboxes').on('change',(val)=>{
+            if(val.isTrusted){
+                switch (val.target.dataset.caption) {
+                    case "Adapt":
+                        strategyAdaptation=="Adapt"?strategyAdaptation=null:strategyAdaptation="Adapt"
+                        break;
+                    case "Prepare":
+                        strategyPreparation=="Prepare"?strategyPreparation=null:strategyPreparation="Prepare"
+                        break;
+                    default:
+                        strategyAnticipation=="Anticipate"?strategyAnticipation=null:strategyAnticipation="Anticipate"
+                        break;
+                }
+                applyFilters()
             }
-            console.log("Adapt: "+strategyAdaptation)
-            console.log("Prepare: "+strategyPreparation)
-            console.log("Anticipate: "+strategyAnticipation)
-            applyFilters()
+        })
+        $('#category-checkboxes').on('change',(val)=>{
+            if(val.isTrusted){
+                if(categories.includes(val.target.dataset.caption))categories=categories.filter((e)=>{ return e !== val.target.dataset.caption })
+                else categories.push(val.target.dataset.caption)
+                console.log(categories)
+                applyFilters()
+            }
+        })
+        $('#relevant-issue-checkboxes').on('change',(val)=>{
+            if(val.isTrusted){
+                if(issues.includes(val.target.dataset.caption))issues=issues.filter((e)=>{ return e !== val.target.dataset.caption })
+                else issues.push(val.target.dataset.caption)
+                applyFilters()
+            }
+        })
+        function applyFilters(){
+            let timeFrame1st=[">=", ['to-number', ["get", 'time']], parseInt(timeFrame[0])],
+            timeFrame2nd=["<=", ['to-number', ["get", 'time']], parseInt(timeFrame[1])],
+            issueFilter=["in", ["get","Issues"],["literal", issues]],
+            categoryFilter=["in", ["get","Category"],["literal", categories]],
+            adaptationFilter=["==",["get","Drought Resilience Strategy Adaptation"],strategyAdaptation],
+            anticipationFilter=["==",["get","Drought Resilience Strategy Anticipation"],strategyAnticipation],
+            preparationFilter=["==",["get","Drought Resilience Strategy Preparation"],strategyPreparation],
+            queryFilter=['all',timeFrame1st,timeFrame2nd,issueFilter,categoryFilter]
+            if(strategyAdaptation==null&&strategyAnticipation==null&&strategyPreparation==null){}
+            else queryFilter.push(adaptationFilter,anticipationFilter,preparationFilter)
+            map.setFilter('point-layer',queryFilter)
         }
-    })
-    $('#category-checkboxes').on('change',(val)=>{
-        if(val.isTrusted){
-            if(categories.includes(val.target.dataset.caption))categories=categories.filter((e)=>{ return e !== val.target.dataset.caption })
-            else categories.push(val.target.dataset.caption)
-            console.log(categories)
+        map.once('style.load',()=>{
+            loadLayers()
             applyFilters()
-        }
-    })
-    $('#relevant-issue-checkboxes').on('change',(val)=>{
-        if(val.isTrusted){
-            if(issues.includes(val.target.dataset.caption))issues=issues.filter((e)=>{ return e !== val.target.dataset.caption })
-            else issues.push(val.target.dataset.caption)
+        })
+        $('#time-slider').on('change',(val)=>{
+            timeFrame=val.detail.val.split(',')
+            $('#slider-return-value').val(val.detail.val)
             applyFilters()
-        }
+        })
     })
-    function applyFilters(){
-        let timeFrame1st=[">=", ['to-number', ["get", 'time']], parseInt(timeFrame[0])],
-        timeFrame2nd=["<=", ['to-number', ["get", 'time']], parseInt(timeFrame[1])],
-        issueFilter=["in", ["get","Issues"],["literal", issues]],
-        categoryFilter=["in", ["get","Category"],["literal", categories]],
-        adaptationFilter=["==",["get","Drought Resilience Strategy Adaptation"],strategyAdaptation],
-        anticipationFilter=["==",["get","Drought Resilience Strategy Anticipation"],strategyAnticipation],
-        preparationFilter=["==",["get","Drought Resilience Strategy Preparation"],strategyPreparation],
-        queryFilter=['all',timeFrame1st,timeFrame2nd,issueFilter,categoryFilter]
-        if(strategyAdaptation==null&&strategyAnticipation==null&&strategyPreparation==null){}
-        else queryFilter.push(adaptationFilter,anticipationFilter,preparationFilter)
-        map.setFilter('point-layer',queryFilter)
-    }
-    map.on('style.load',()=>{
-        loadLayers()
-        applyFilters()
-    })
-    $('#time-slider').on('change',(val)=>{
-        timeFrame=val.detail.val.split(',')
-        $('#slider-return-value').val(val.detail.val)
-        applyFilters()
-    })
+    
 })
 
 $('#filter-button').on('click',()=>{
     if(hideFilter){
         $('#filter-content').css('display','none')
+        $('#filter-container').css({'display':'none'})
+        $('#map').css('height','100vh')
+        map.resize();
         $('#filter-button').html(`
             <div class="row g-0 text-center">
                 <div class="cell-3"><i class="bi bi-chevron-compact-up"></i></div>
@@ -179,6 +219,9 @@ $('#filter-button').on('click',()=>{
         hideFilter=false
     }else{
         $('#filter-content').css('display','flex')
+        $('#filter-container').css({'display':'block'})
+        $('#map').css('height','75vh')
+        map.resize();
         $('#filter-button').html(`
             <div class="row g-0 text-center">
                 <div class="cell-3"><i class="bi bi-chevron-compact-down"></i></div>
